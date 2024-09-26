901e4ca420976014b5f408ecaadb56535fc2f6a3
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2024-09-26 14:51:17 +0100

    header mobile markup fix & filter-and-sorting component create

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './styles/app.scss'
 import Header from "./components/Header/Header.tsx";
+import FiltersAndSorting from "./components/FiltersAndSorting/FiltersAndSorting.tsx";
+
 
 function App() {
 
     return (
         <>
             <Header/>
+            <FiltersAndSorting/>
         </>
     )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './header.scss'
 import logo from '../../assets/logo.png'
-import {IoCartSharp, IoPersonCircleSharp} from "react-icons/io5";
+import {IoCartSharp, IoMenu, IoPersonCircleSharp} from "react-icons/io5";
+import {RxDividerVertical} from "react-icons/rx";
 
 function Header() {
     return (
@@ -14,6 +15,10 @@ function Header() {
                     <a className='header__title-link'><h1 className='header__title'>Saladly</h1></a>
                     {/*<h2 className='header__subtitle'>The Tastiest and Largest Salads on Earth!</h2>*/}
                 </div>
+
+                <button className='header__mobile-menu'>
+                    <IoMenu className='header__mobile-burger'/>
+                </button>
             </div>
 
             {/*// TODO SEARCH BAR AT HEADER CENTER*/}
@@ -21,13 +26,12 @@ function Header() {
             <input className='header__search-input' type="text" placeholder='🔍  Search for a salad...'/>
 
             <div className='header__buttons'>
-                <button className='header__login-button' type='button'> <span className='header__login-text'>Account |</span> <IoPersonCircleSharp
+                <button className='header__login-button' type='button'> <span className='header__login-text'>Account</span> <RxDividerVertical/>  <IoPersonCircleSharp
                     style={{width: '30px', height: '30px'}}/></button>
-                <button className='header__cart-button' type='button'><span className='header__cart-text'>100$ |</span>
+                <button className='header__cart-button' type='button'><span className='header__cart-text'>100$</span> <RxDividerVertical/>
                     <IoCartSharp style={{width: '30px', height: '30px'}}/></button>
             </div>
 
-            {/*<h3 className='header__subtitle-hidden'>The Tastiest and Largest Salads <br/> on Earth! Order now.</h3>*/}
 
         </header>
     );
